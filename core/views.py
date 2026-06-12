5cb6fc2a00f0d2f58365d40bc9a0e3c75eadfff6
dodiwangkarabi-lang <dodiwangkarabi@gmail.com>
2026-06-13 07:36:24 +0800

    modif: dashboard.html

--- core/views.py
+++ core/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+# services
+from core.services.dashboard_service import StatistikService
+
 @login_required(login_url="accounts:login")
 def index(request):
-    return render(request, "core/dashboard/dashboard.html")
+    context = {
+        "statistik_siswa": StatistikService().statistik_siswa(),
+    }
+    return render(request, "core/dashboard/dashboard.html", context)
